37a03660e6d474f806f618c8f3b8a87a7af1d089
Aly <38168385+alycapps@users.noreply.github.com>
2018-06-24 15:32:09 -0400

    adding basic content to html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,32 @@
-"Which US President was shot while giving a speech and still finished the hour and half speech? "
-Theodore Roosevelt
-"I don't know whether you fully understand that I have just been shot. I give you my word, I do not care a rap about being shot; not a rap!"
+// "Which US President was shot while giving a speech and still finished the hour and half speech? "
+// Theodore Roosevelt
+// "I don't know whether you fully understand that I have just been shot. I give you my word, I do not care a rap about being shot; not a rap!"
 
-"About how much pizza do Americans eat every day?"
-100 Acres or 350 slices per second
+// "About how much pizza do Americans eat every day?"
+// 100 Acres or 350 slices per second
 
-"What percentage of Americans own dogs?"
-44%
+// "What percentage of Americans own dogs?"
+// 44%
 
-"What year was America founded?"
-1776
+// "What year was America founded?"
+// 1776
 
-"What percentage of Americans live in Wyoming?"
-0.18%
+// "What percentage of Americans live in Wyoming?"
+// 0.18%
 
-"What American company invented Easy Cheese?"
-Mondelēz International
+// "What American company invented Easy Cheese?"
+// Mondelēz International
 
-"In what US City did Comic-Con start?"
-San Diego, California
+// "In what US City did Comic-Con start?"
+// San Diego, California
+
+
+$(document).ready(function() {
+
+
+
+
+
+
+    
+}

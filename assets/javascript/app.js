3a5244f0258800995a969c5bcd56115e99bf379d
Aly <38168385+alycapps@users.noreply.github.com>
2018-07-04 17:12:58 -0400

    fixed random location of answer choices

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+$(document).ready(preparepage());
+
     var correctcount = 0;
     var wrongcount = 0;
     var skippedcount = 0;
@@ -8,7 +9,7 @@ $(document).ready(function() {
     var q1 = {
         Question:"Which US President was shot while giving a speech and still finished the hour and half speech?",
         Answer: "Theodore Roosevelt",
-        RedHerrings: ["Theodore Roosevelt", "Abraham Lincoln", "Ulysses S. Grant", "Andrew Jackson"],
+        RedHerrings: ["Abraham Lincoln", "Ulysses S. Grant", "Andrew Jackson"],
     };
 
     var q2 = {
@@ -47,20 +48,22 @@ $(document).ready(function() {
         RedHerrings: ["0.57%", "1.21%", "0.92%"],
     };
 
-    // hit start button to go to first question
-    $(".start").on("click", function() {
-        timerload();
-        qpgsetup();
-        console.log("start button clicked");
-    });
+// hit start button to go to first question
+    function preparepage() {
+        $(".start").on("click", function() {
+            timerload();
+            qpgsetup();
+            console.log("start button clicked");
+        });
+    };
 
     function timerload() {
         //timer countdown
         var interval = setInterval(countdown, 1000);
         function countdown() {
             seconds--;
-            console.log("countdown function ran");
-            console.log("seconds" + seconds);
+            // console.log("countdown function ran");
+            // console.log("seconds" + seconds);
             // $("timerlocation").html(seconds + "seconds");
             $(".timerlocation").html("<strong>" + seconds + "</strong>" + " seconds left");
 
@@ -78,20 +81,27 @@ $(document).ready(function() {
     function qpgsetup() {
         $(".questionlocation").text(q1.Question);
         ///randomize locations of answer choices
-        //run length of answer choices
-        // for (a=0, a<4, a++) {
-            // for (i=0; i=q1.RedHerrings.length - 1; i++) {
-            //     var a = Math.random(Math.floor() * 3);
-            //     $(".ansloc" + i).text(q1.RedHerrings[a]);
+        var anslocarray = [0,0,0,0];
+        for (i = 0; i < 4; i++) {
+            var x = Math.floor(Math.random() * 4) + 1;
+            var v = anslocarray.indexOf(x);
+            while (v >= 0) {
+                console.log("loop started - value is " + x + " and array looks like: " + anslocarray)
+                x = Math.floor(Math.random() * 4) + 1;
+                v = anslocarray.indexOf(x);
+                console.log(v);
+            }
+            anslocarray[i] = x;
+        }
+        console.log(anslocarray);
 
-            // };
+        $(".ansloc"+anslocarray[0]).text(q1.Answer);
+        $(".ansloc"+anslocarray[1]).text(q1.RedHerrings[0]);
+        $(".ansloc"+anslocarray[2]).text(q1.RedHerrings[1]);
+        $(".ansloc"+anslocarray[3]).text(q1.RedHerrings[2]);
 
-        // };
-        $(".ansloc1").text(q1.Answer);
-        $(".ansloc2").text(q1.RedHerrings[0]);
-        $(".ansloc3").text(q1.RedHerrings[1]);
-        $(".ansloc4").text(q1.RedHerrings[2]);
-    }
+        $(".ansloc"+anslocarray[0]).id = "answer";
+    };
 
     //page 1 run questiondisplay function
 
@@ -102,5 +112,3 @@ $(document).ready(function() {
     //if time runs then run questiondisplay function and add 1 to skippedcount
 
     //after all questions display summary page
-
-});
